30214277e9f0447548550ea0cbc56fe9b7779dc6
Aruna kadugu <arunakumari.7k@gmail.com>
2017-11-06 15:23:40 -0500

    Initial commit

---
+++ src/main/java/my/aruna/springboot2/HomeController.java
@@ -0,0 +1,14 @@
+package my.aruna.springboot2;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+@Controller
+public class HomeController {
+    @RequestMapping("/")
+    public String homepage(Model model){
+        model.addAttribute("myvar", "say hello to the people.");
+        return "hometemplate";
+    }
+
+}
